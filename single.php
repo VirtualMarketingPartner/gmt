bf5e16958761057c2611477b9ad23afa64e6ff73
Taylor Desens <taylor.desens@gmail.com>
2020-11-17 20:57:02 -0500

    Remove redundant files

--- single.php
+++ single.php
@@ -1,21 +1,27 @@
 
 <?php get_header(); ?>
 
-<?php if( get_field('hero') ): ?>
-	<div class="jumbotron-fluid hero" style="background-image:url(<?php the_field('hero'); ?>);" ></div>
-<?php endif; ?>
-<div class="content" >
-	<div class="container" >
-		<div class="row" >
-			<div class="col-sm" >
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php get_template_part('/partials/hero'); ?>
+
+<section class="container-fluid angle post" >
+		<div class="container" >
+			
+			<div class="row" >
+				<div class="col-sm-12 col-md-8" >
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					<?php the_date(); ?>
 					<h1><?php the_title(); ?></h1>
 					<?php the_content(); ?>
-				<?php endwhile; endif; ?>
-			</div><!-- .col-sm -->
-		</div><!-- .row -->
-	</div><!-- .container -->
-</div><!-- .content -->
+					<?php endwhile; endif; ?>
+				</div><!-- .col -->
+				
+				<div class="col-sm-12 offset-md-1 col-md-3" >
+					<h4><?php the_field('recent_news','options'); ?></h4>
+					[The Sidebar will go here.]
+				</div><!-- .col -->
+			</div><!-- .row -->
+		</div><!-- .container -->
+	</section><!-- .post -->
 
 
 <?php get_footer(); 
