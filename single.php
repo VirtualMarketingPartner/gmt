86fa415b20192d5e7792aa49cddda0a6bc6181b3
Taylor Desens <taylor.desens@gmail.com>
2020-12-28 12:31:24 -0500

    Update insights to allow "author" bio

--- single.php
+++ single.php
@@ -14,21 +14,53 @@
 						<h1><?php the_title(); ?></h1>
 						<?php the_content(); ?>
 					</div><!-- .text-wrapper -->
-					<?php endwhile; endif; ?>
+					<?php wp_reset_postdata(); endwhile; endif; ?>
+					
+					<?php if(get_field('author')): ?>
+					<?php $authors = get_field('author'); if( $authors ): ?>
+					<?php foreach( $authors as $post ): setup_postdata($post); ?>
+					<div class="row" >
+						<div class="col" >
+							<div class="card bottom" >
+								<div class="card-body" >
+									<div class="row" >
+										<div class="col-12 col-md-4" >
+											<?php if(have_rows('team_member')): while(have_rows('team_member')): the_row(); ?>
+											<div class="image-wrapper circle" style="background-image:url(<?php the_sub_field('image'); ?>);" ></div>
+											<?php endwhile; endif; ?>
+										</div><!-- .col -->
+										<div class="col-12 col-md-8" >
+											<div class="text-wrapper" >
+												<h5><?php the_title(); ?></h5>
+												<?php the_content(); ?>
+											</div><!-- .text-wrapper -->
+										</div><!-- .col -->
+									</div><!-- .row -->
+								</div><!-- .card-body -->
+							</div><!-- .card -->						
+						</div><!-- .col -->
+					</div><!-- .row -->
+					<br>
+					<?php endforeach; wp_reset_postdata(); endif; ?>
+					<?php endif; ?>
+
 					
 					<?php if(get_field('post_cta','options')): ?>
-					<div class="card bottom" id="post_cta" >
-						<div class="card-body" >
+					<div id="post_cta" >
+						
 							<?php the_field('post_cta','options'); ?>
-						</div><!-- .card-body -->
-					</div><!-- .card -->
+						
+					</div><!-- #post_cta -->
 					<?php endif; ?>
+					
+					
 				</div><!-- .col .post-body -->
 				
 				<div class="col-12 offset-md-1 col-md-4" >
 					<?php get_template_part('/partials/post_sidebar'); ?>
 				</div><!-- .col -->
 			</div><!-- .row -->
+			
 		</div><!-- .container -->
 	</section><!-- .post -->
 
