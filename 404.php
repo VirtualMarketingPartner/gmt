0d52d101425e2cb3c53b190f5367482d56bb2eb2
Taylor Desens <taylor.desens@gmail.com>
2021-01-25 16:14:00 -0500

    Make 404 editable with flexible fields

--- 404.php
+++ 404.php
@@ -3,24 +3,24 @@
 ?>
 
 <?php get_header(); ?>
-
-
-<div id="content" >
-	<div class="container" >
-		<div class="row" >
-			<div class="col-sm" >
-				<h1>Uh Oh</h1>
-				<h2>It's not you, it's me.  Well, in this case it's us.</h2>
-
-				<blockquote>
-					Now, here, you see, it takes all the running you can do, to keep in the same place. If you want to get somewhere else, you must run at least twice as fast as that!
-					<p class="caption">The Red Queen, Alice in Wonderland</p>
-				</blockquote>
-
-			</div><!-- .col-sm -->
-		</div><!-- .row -->
-	</div><!-- .container -->
-</div><!-- #content -->
-
-
+<div class="content" id="flexi">
+	
+	<?php if(have_rows('error_page','options')): while(have_rows('error_page','options')): the_row();
+	$bgImage = get_sub_field('bg_image'); 
+	?>
+	<section class="container-fluid <?php the_sub_field('bg'); ?> <?php if( get_sub_field('bg') != 'bg' ){ echo 'angle'; } ?>" style="background-image:url(<?php if( get_sub_field('bg') == 'bg'){ echo $bgImage; } ?>);" >
+		<?php if( get_row_layout() == 'full_flex'): ?>
+		<?php get_template_part('/partials/flex/full_flex'); ?>
+		<?php elseif( get_row_layout() == 'column_flex'): ?>
+		<?php get_template_part('/partials/flex/column_flex'); ?>
+		<?php elseif( get_row_layout() == 'cards_flex'): ?>
+		<?php get_template_part('/partials/flex/cards_flex'); ?>
+		<?php elseif( get_row_layout() == 'combo_flex'): ?>
+		<?php get_template_part('/partials/flex/combo_flex'); ?>		
+		<?php endif; ?>
+	</section><!-- .container-fluid -->
+	
+	<?php endwhile; endif; ?>
+	
+</div><!-- #flexi -->
 <?php get_footer(); 
