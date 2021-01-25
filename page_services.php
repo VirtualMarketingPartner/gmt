54c1dca0fbb8b673f10b4e49f1d7d42c8abf5e4a
Taylor Desens <taylor.desens@gmail.com>
2021-01-25 16:12:45 -0500

    Remove the "capabilites" and add the "quotes" to the Services page

--- page_services.php
+++ page_services.php
@@ -63,11 +63,7 @@
 								</div><!-- .tab-pane -->
 								<?php $serviceTabCount++; endforeach; wp_reset_postdata(); ?>
 							</div><!-- .#service-content -->
-						</div><!-- .col -->
-						
-						
-						
-						
+						</div><!-- .col -->	
 						
 					</div><!-- .row -->
 				</div><!-- #service_tabs -->
@@ -75,31 +71,42 @@
 		</section><!-- .services -->
 		<?php endwhile; endif; ?>
 		
-		<?php if(have_rows('approach')): while(have_rows('approach')): the_row(); ?>
-		<section class="container-fluid angle approach" >
+		<?php if(have_rows('testimonials')): while(have_rows('testimonials')): the_row(); ?>
+		<section class="container-fluid angle testimonials" >
 			<div class="container" >
-				<div class="row" >
-					<div class="col-12 text-center" >
+				<div class="row vcenter" >
+					<div class="col-12 col-md-5 " >
+						<div class="pattern bottom left animate" >
+							<div class="image-wrapper circle blocker" style="background-image:url(<?php the_sub_field('image'); ?>);"></div>
+							<div class="pattern-block" ></div>
+						</div><!-- .pattern -->
+					</div><!-- .col -->
+
+					<div class="col-12 col-md-7 animate" >
 						<h2><?php the_sub_field('header'); ?></h2>
-						<?php if(have_rows('list')): ?>
-						<div class="row card-row" >
-							<?php while(have_rows('list')): the_row(); ?>
-							<div class="col-12 col-md-4" >
-								<div class="card bottom animate" >
-									<div class="card-body text-left" >
-										<div class="image-wrapper icon" style="background-image:url(<?php the_sub_field('icon'); ?>);"></div> 
-										<h3><?php the_sub_field('title'); ?></h3>
-										<?php the_sub_field('content'); ?>
-									</div><!-- .card-body -->
-								</div><!-- .card -->
-							</div><!-- .col -->
-							<?php endwhile; ?>
-						</div><!-- .row -->
+						<?php if(have_rows('quotes')): $quote=0; $quoteIndicator=0; ?>
+						<div id="quoteCarousel" class="carousel slide carousel-fade" data-ride="carousel" >
+							<ol class="carousel-indicators" >
+								<?php while( have_rows('quotes')): the_row(); ?>
+								<li data-target="#quoteCarousel" data-slide-to="<?php echo $quoteIndicator; ?>" class="<?php if($quoteIndicator==0){echo 'active'; } ?>" ></li>
+								<?php $quoteIndicator++; endwhile; ?>
+							</ol>
+							<div class="carousel-inner" >
+							<?php while(have_rows('quotes')): the_row(); ?>
+								<div class="carousel-item <?php if($quote==0){echo 'active';} ?>" >
+									<p><?php the_sub_field('quote'); ?></p>
+									<p class="author" ><?php the_sub_field('author'); ?></p>
+									<p class="company" ><?php the_sub_field('company'); ?></p>
+								</div><!-- .carousel-item -->
+							<?php $quote++; endwhile; ?>
+							</div><!-- .carousel-inner -->
+						</div><!-- #quoteCarousel -->
 						<?php endif; ?>
+
 					</div><!-- .col -->
 				</div><!-- .row -->
 			</div><!-- .container -->
-		</section><!-- .approach -->
+		</section><!-- .testimonials -->
 		<?php endwhile; endif; ?>
 		
 		<?php if(have_rows('fees')): while(have_rows('fees')): the_row(); ?>
