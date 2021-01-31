cad962dbf7e72d45c853378139f4c3d2d7440ec0
Taylor Desens <taylor.desens@gmail.com>
2021-01-31 12:21:06 -0500

    Update hero for wider space and H1

--- partials/hero.php
+++ partials/hero.php
@@ -4,10 +4,10 @@
 <section class="container-fluid bg hero" style="background-image:url(<?php the_sub_field('hero_image'); ?>); background-position:<?php the_sub_field('hero_position'); ?>" >
 	<div class="container" >
 		<div class="row" >
-			<div class="col-12 col-md-6" >
+			<div class="col-12 col-md-8" >
 				<div class="text-wrapper animate" >
 					<p class="all-caps" ><?php the_sub_field('sub_header'); ?></p>
-					<h2><?php the_sub_field('header'); ?></h2>
+					<h1><?php the_sub_field('header'); ?></h1>
 					<?php if( get_sub_field('label')): ?>
 					<a href="<?php the_sub_field('link'); ?>" class="button" ><?php the_sub_field('label'); ?> <i class="fas fa-long-arrow-right"></i></a>
 					<?php endif; ?>
