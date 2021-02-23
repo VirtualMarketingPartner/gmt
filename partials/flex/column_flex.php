9e1ba38d985fd7d7c233eb857ec4c9270b73bb86
Taylor Desens <taylor.desens@gmail.com>
2021-02-23 11:42:21 -0500

    Update columns to let user decide size

--- partials/flex/column_flex.php
+++ partials/flex/column_flex.php
@@ -7,13 +7,13 @@
 					$count = count(get_sub_field('columns')); 
 					if(have_rows('columns')): ?>
 				<div class="row" >
-					<?php while(have_rows('columns')): the_row(); ?>
+					<?php while(have_rows('columns')): the_row(); 
+					$colSize = get_sub_field('column_size'); ?>
 					<div class="col <?php 
-								if($count %6 == 0){ echo "col-md-4"; }else  
-								if($count %5 == 0){ echo "col-md-4"; }else
-								if($count %4 == 0){ echo "col-md-3"; }else
-								if($count %3 == 0){ echo "col-md-4"; }else
-								if($count %2 == 0){ echo "col-md-6"; }?>" >
+								if($colSize == 1){ echo "col-md-12"; }else  
+								if($colSize == 2){ echo "col-md-6"; }else  
+								if($colSize == 3){ echo "col-md-4"; }else 
+								if($colSize == 4){ echo "col-md-3"; }?>" style="margin-bottom:100px;" >
 						<?php the_sub_field('content'); ?>
 					</div><!-- .col -->
 					<?php endwhile; ?>
