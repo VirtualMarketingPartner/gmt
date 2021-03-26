d4057e6c215706c7721c3845500de8988ba48e9f
Taylor Desens <taylor.desens@gmail.com>
2021-03-25 21:26:26 -0400

    Remove quote attributes & replace "header" with "wysiwyg" content

--- partials/flex/quote_flex.php
+++ partials/flex/quote_flex.php
@@ -1,6 +1,11 @@
 <?php if(have_rows('testimonials')): while(have_rows('testimonials')): the_row(); ?>
 <section class="container-fluid angle testimonials" >
 	<div class="container" >
+		<div class="row" >
+			<div class="col" >
+				<?php the_sub_field('content'); ?>
+			</div><!-- .col -->
+		</div><!-- .row -->
 		<div class="row vcenter" >
 			<div class="col-12 col-md-4" >
 				<div class="pattern bottom left animate" >
@@ -10,7 +15,6 @@
 			</div><!-- .col -->
 
 			<div class="col-12 col-md-7 animate" >
-				<h2><?php the_sub_field('header'); ?></h2>
 				<?php if(have_rows('quotes')): $quote=0; $quoteIndicator=0; ?>
 				<div id="quoteCarousel" class="carousel slide" data-ride="carousel" >
 					<ol class="carousel-indicators" >
@@ -21,9 +25,7 @@
 					<div class="carousel-inner" >
 					<?php while(have_rows('quotes')): the_row(); ?>
 						<div class="carousel-item <?php if($quote==0){echo 'active';} ?>" >
-							<p><?php the_sub_field('quote'); ?></p>
-							<p class="author" ><?php the_sub_field('author'); ?></p>
-							<p class="company" ><?php the_sub_field('company'); ?></p>
+							<?php the_sub_field('quote'); ?>
 						</div><!-- .carousel-item -->
 					<?php $quote++; endwhile; ?>
 					</div><!-- .carousel-inner -->
