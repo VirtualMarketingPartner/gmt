789d6117e96b2ce0f2c3a9b24e86d4ee04e8efd3
Taylor Desens <taylor.desens@gmail.com>
2021-03-25 21:33:36 -0400

    Update hero to include variables

--- partials/hero.php
+++ partials/hero.php
@@ -1,17 +1,18 @@
-<?php if(have_rows('hero')): while(have_rows('hero')): the_row(); ?>
-<?php if(get_sub_field('hero_image')): ?>
+<?php if(have_rows('hero')): while(have_rows('hero')): the_row(); 
+$bgImage = get_sub_field('hero_image');
+$minHeight = get_sub_field('hero_height');
+$imagePos = get_sub_field('hero_position');
+?>
 
-<section class="container-fluid bg hero" style="background-image:url(<?php the_sub_field('hero_image'); ?>); background-position:<?php the_sub_field('hero_position'); ?>" >
+<?php if(get_sub_field('hero_image')): ?>
+<section class="container-fluid bg hero" style="background-image:url(<?php echo $bgImage; ?>); background-position:<?php echo $imagePos; ?>; min-height:<?php echo $minHeight; ?>px;" >
 	<div class="container" >
 		<div class="row" >
 			<div class="col-12 col-md-8" >
-				<div class="hero-text animate" >
+				<div class="hero-text animate" style="min-height:<?php echo $minHeight; ?>px;" >
 					<div class="content" >
 						<p class="all-caps" ><?php the_sub_field('sub_header'); ?></p>
 						<h1><?php the_sub_field('header'); ?></h1>
-						<?php if( get_sub_field('label')): ?>
-						<a href="<?php the_sub_field('link'); ?>" class="button" ><?php the_sub_field('label'); ?> <i class="fas fa-long-arrow-right"></i></a>
-						<?php endif; ?>
 					</div><!-- .content -->
 				</div><!-- .hero-text -->
 			</div><!-- .col -->
@@ -24,7 +25,14 @@
 <section class="container-fluid hero-spacer" >
 	<div class="container" >
 		<div class="row" >
-			<h1><?php the_title(); ?></h1>
+			<div class="col-12 col-md-8" >
+				<div class="hero-text animate" style="min-height:<?php echo $minHeight; ?>px;" >
+					<div class="content" >
+						<p class="all-caps" ><?php the_sub_field('sub_header'); ?></p>
+						<h1><?php the_sub_field('header'); ?></h1>
+					</div><!-- .content -->
+				</div><!-- .hero-text -->
+			</div><!-- .col -->
 		</div><!-- .row -->
 	</div><!-- .container -->
 </section><!-- .hero-spacer -->
